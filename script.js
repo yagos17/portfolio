8f0a3684f481608eb2e3c41aadc693279d8c43e1
Yago Sona <yagolee70@gmail.com>
2024-09-10 22:38:58 -0300

    Update index.html script.js

--- script.js
+++ script.js
@@ -16,13 +16,18 @@ const sr = ScrollReveal({
 });
 
 // Configura as animações de rolagem para diferentes elementos
+/* Home Section */
 sr.reveal('.logo', { delay: 200, origin: 'left' });
 sr.reveal('.menu-btn', { delay: 600, origin: 'right' });
-
 sr.reveal('.home-text span', { delay: 600, origin: 'top' });
 sr.reveal('.home-text h1', { delay: 680, origin: 'left' });
 sr.reveal('.home-text p', { delay: 750, origin: 'right' });
 sr.reveal('.main-btn', { delay: 860, origin: 'left' });
-
 sr.reveal('.share', { delay: 950, origin: 'bottom' });
 sr.reveal('.home-img', { delay: 1000, origin: 'right' });
+
+/* About Section */
+sr.reveal('.about-text h1', { delay: 680, origin: 'left' });
+sr.reveal('.about-text p', { delay: 750, origin: 'right' });
+sr.reveal('.about-img', { delay: 1000, origin: 'right' });
+
